385cedcf7b84587e17d0a89f9ef8a0d88d152632
Владимир Сафронов <Tyuiu.SafronovVE@outlook.com>
2025-09-12 17:40:00 +0500

    Task1

---
+++ Tyuiu.SafronovVE.Sprint1.Task1.V29.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Tyuiu.SafronovVE.Sprint1.Task1.V29.Lib;
+namespace Tyuiu.SafronovVE.Sprint1.Task1.V29.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+
+            double a = 3.0;
+            double b = 2.0;
+            double c = 1.0;
+
+            var res = ds.Calculate(a, b, c);
+            Assert.AreEqual(1, res);
+
+        }
+    }
+}
