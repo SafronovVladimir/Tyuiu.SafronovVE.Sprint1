2eddf407d5777a4fe47bb66295bce4a21d7dd966
Владимир Сафронов <Tyuiu.SafronovVE@outlook.com>
2025-09-12 19:01:22 +0500

    Task2

---
+++ Tyuiu.SafronovVE.Sprint1.Task2.V21.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.SafronovVE.Sprint1.Task2.V21.Lib;
+namespace Tyuiu.SafronovVE.Sprint1.Task2.V21.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            int value = 2;
+            int valueTwo = 3;
+            var res = ds.CalculateRectangleSquare(value, valueTwo);
+            Assert.AreEqual(6, res);
+        }
+    }
+}
---
+++ Tyuiu.SafronovVE.Sprint1.Task2.V21/Program.cs
@@ -0,0 +1,41 @@
+﻿using Tyuiu.SafronovVE.Sprint1.Task2.V21.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #1 | Выполнил: Сафронов В. Е. | ИБКСб-25-1";
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #1                                                               *");
+        Console.WriteLine("* Тема: Арифметические операторы в C#                                     *");
+        Console.WriteLine("* Задание #2                                                              *");
+        Console.WriteLine("* Вариант #21                                                             *");
+        Console.WriteLine("* Выполнил: Сафронов Владимир Евгеньевич | ИБКСб-25-1                     *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+        Console.WriteLine("* выполняет указанные расчёты и печатает результат на экране.             *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+        
+        int value, valueTwo;
+        
+        Console.WriteLine("Введите длину первой стороны (целое число): ");
+        value = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Введите длину второй стороны (целое число): ");
+        valueTwo = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine(ds.CalculateRectangleSquare(value, valueTwo));
+
+        Console.ReadLine();
+    }
+}
