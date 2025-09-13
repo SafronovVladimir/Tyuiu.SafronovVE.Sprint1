d0fbcf4ee8786c4aa0a43ef0106f1e7825bf5bd6
Владимир Сафронов <Tyuiu.SafronovVE@outlook.com>
2025-09-13 13:45:55 +0500

    Task3

---
+++ Tyuiu.SafronovVE.Sprint1.Task3.V3.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.SafronovVE.Sprint1.Task3.V3.Lib;
+namespace Tyuiu.SafronovVE.Sprint1.Task3.V3.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+
+            double length = 2;
+            double width = 4;
+            double height = 8;
+
+            var res = ds.ParallelepipedVolume(length, width, height);
+            Assert.AreEqual(64, res);
+        }
+    }
+}
---
+++ Tyuiu.SafronovVE.Sprint1.Task3.V3/Program.cs
@@ -0,0 +1,44 @@
+﻿using Tyuiu.SafronovVE.Sprint1.Task3.V3.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #1 | Выполнил: Сафронов В. Е. | ИБКСб-25-1";
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #1                                                               *");
+        Console.WriteLine("* Тема: Операторы составного присваивания                                 *");
+        Console.WriteLine("* Задание #3                                                              *");
+        Console.WriteLine("* Вариант #3                                                              *");
+        Console.WriteLine("* Выполнил: Сафронов Владимир Евгеньевич | ИБКСб-25-1                     *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+        Console.WriteLine("* выполняет указанные расчёты и печатает результат на экране.             *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        double length, width, height;
+
+        Console.WriteLine("Введите длину: ");
+        length = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Введите ширину: ");
+        width = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Введите высоту: ");
+        height = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine(ds.ParallelepipedVolume(length, width, height));
+
+        Console.ReadLine();
+    }
+}
