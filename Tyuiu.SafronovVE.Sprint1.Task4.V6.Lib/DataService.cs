a9e64569aa92e6754082d24e0b07906b73e7b6f6
Владимир Сафронов <Tyuiu.SafronovVE@outlook.com>
2025-09-13 14:23:57 +0500

    Task4

---
+++ Tyuiu.SafronovVE.Sprint1.Task4.V6.Lib/DataService.cs
@@ -0,0 +1,12 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+namespace Tyuiu.SafronovVE.Sprint1.Task4.V6.Lib
+{
+    public class DataService : ISprint1Task4V6
+    {
+        public double Calculate(double x, double y)
+        {
+            var res = Math.Round(Math.Abs(Math.Pow(y, 2) - x) / (x * y), 3);
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.SafronovVE.Sprint1.Task4.V6.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.SafronovVE.Sprint1.Task4.V6.Lib;
+namespace Tyuiu.SafronovVE.Sprint1.Task4.V6.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x = 4;
+            double y = 4;
+            double wait = 0.75;
+            var res = ds.Calculate(x, y);
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
